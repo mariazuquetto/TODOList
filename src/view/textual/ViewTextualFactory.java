9b2e82e5d6fb3399ebabdb742f4b66bb77ab0aa9
mariazuquetto <maduzuquetto@gmail.com>
2025-11-26 02:46:20 -0300

    Mais coisas

---
+++ src/view/textual/ViewTextualFactory.java
@@ -0,0 +1,13 @@
+package view.textual;
+
+import model.Programa;
+import view.IFabricaVisual;
+import view.IMenuView;
+
+public class ViewTextual implements IFabricaVisual {
+
+    @Override
+    public IMenuView mostrarMenu(Programa programa) {
+        return null;
+    }
+}
